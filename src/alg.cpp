21ae174dfce2bda538dbb822d5fe6eab57ea8b7a
OwlInside <78905764+OwlInside@users.noreply.github.com>
2021-04-11 09:16:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 for (int i = 0, i < len, i++) {
         for (int j = i, j < len - 1 - i, j++) {
                 if (arr[j] > arr[j+1]) {
-                        swap (arr[j], arr[j + 1]);
+                        swap(arr[j], arr[j + 1]);
                 }
         }
 }
@@ -23,7 +23,7 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
         int count = 0;
-        for (int i = 0, i < len, i++){
+        for (int i = 0, i < len, i++) {
                 for (int j = len - 1, j < len, j--) {
                         if (arr[i] + arr[j] == value) {
                                 count++;
