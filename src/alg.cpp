7b42417b8915763324474a07193faf2b5ec3d4f2
OwlInside <78905764+OwlInside@users.noreply.github.com>
2021-04-12 20:53:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,28 @@
 // Copyright 2021 NNTU-CS
 int sort(int *arr, int len) {
-for (int i = 0, i < len, i++) {
-        for (int j = i; j < len - 1 - i; j++) {
-                if (arr[j] > arr[j+1]) {
-                        swap(arr[j], arr[j + 1]);
+for (int i = 0; i < len; i++) {
+        for (int j = (len - i); j > i; j++) {
+                if (arr[j - 1] > arr[j]) {
+                        int tmp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = tmp;
                 }
         }
 }
 }
+int binsearch(int *arr, int r, int l, int x) {
+        if (r >= l) {
+                int midd = l + (r - l) / 2
+                    if (arr midd == x) {
+                            return midd;
+                    }
+        }
+        if (arr[midd] > x) 
+                return binsearch(*arr, l, midd - 1, x);
+        else
+                return binseach(*arr, r, midd + 1, x);
+}
+        
 int countPairs1(int *arr, int len, int value) {
         int count = 0;
         for (int i = 0; i < len; i++) {
@@ -44,7 +59,7 @@ int countPairs3(int *arr, int len, int value) {
                 r--;
         }
         while (l != r) {
-                count += cbinsearch(arr, len, value - arr[l], l + 1);
+                count += binsearch(arr, len, value - arr[l], l + 1);
                 l++;
         }
   return count;
